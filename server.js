02bdf22e7ee1630eb3d609a5f35e7b14d5d11611
Andrew <andrewg87@gmail.com>
2022-12-27 10:09:16 -0500

    got the htmlroute.js file up and running

--- server.js
+++ server.js
@@ -3,15 +3,12 @@ const path = require('path');
 
 const PORT = process.env.port || 3001;
 
-const app = express();
+const app = require('./routes/htmlRoute.js')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/', (req, res) => 
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
 
 
 
