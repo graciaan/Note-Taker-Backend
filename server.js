3838881f88b0c07fc5c0af11681f996bbfbb0519
Andrew <andrewg87@gmail.com>
2022-12-27 20:37:36 -0500

    tinkered quite a bit and realized I couldnt get it to work with seperate route files, added everything to server.js and it's starting to work a whole lot better. need to revisit getting seperate route files working. until then, sticking to everything on server.js, deleted the route folder and file

--- server.js
+++ server.js
@@ -1,16 +1,70 @@
 const express = require('express');
-const path = require('path');
-
 const PORT = process.env.port || 3001;
+// const api = require('./routes/apiRoute.js')
+const app = express();
+const path = require("path");
+const fs = require('fs');
+const uuid = require('./helpers/uuid')
 
-const api = require('./routes/apiRoute')
-const app = require('./routes/htmlRoute')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/api/notes', api);
+// app.use('/api/notes', app);
 
 app.use(express.static('public'));
+
+app.get('/', (req, res) => 
+  res.sendFile(path.join(__dirname, './public/index.html'))
+);
+
+app.get('/notes', (req, res) => 
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+);
+
+
+app.get('/api/notes', (req, res) => {
+  res.status(200).json(`${req.method} request received to get notes`);
+  console.info(`${req.method} request received to get notes`);
+});
+
+app.post('/api/notes', (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+  const {title, text} = req.body;
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuid(),
+    };
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedNotes = JSON.parse(data);
+        parsedNotes.push(newNote);
+
+        fs.writeFile(
+          './db/db.json',
+          JSON.stringify(parsedNotes, null, 4),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Successfully updated notes!')
+        );
+      }
+    });
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in posting note');
+  }
+});
 
 
 app.listen(PORT, () =>
